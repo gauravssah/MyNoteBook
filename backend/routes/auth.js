9e655cb37dea884a8f22f9f2e45ffa1419edc95b
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-20 16:37:25 +0530

    data getting on mongodb test server, validatiom added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 const user = User
 
 // Creat a user using: POST "/api/auth". doesn't require Auth
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name', "Enter a valid Name").isLength({ min: 3 }),
+    body('email', "Enter a valid Email id").isEmail(),
+    body('password', "Password must be atlist 5 character").isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: "please enter valid email id", message: err.message })
+        })
 })
 
 
