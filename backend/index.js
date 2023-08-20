42bb4b131f4fdf3da4c0a26967d7202fdbc23ef3
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-20 12:23:41 +0530

    direct path acces in index.js 'not recomended ok'

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.send('Hello Gaurav!')
 })
 
+app.get('/api/login', (req, res) => {
+    res.send('Hello Gaurav Login Now!')
+})
+
+app.get('/api/Singin', (req, res) => {
+    res.send('Hello Gaurav Singin Now!')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port at http://localhost:${port}`)
 })
