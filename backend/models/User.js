2dd2846af75c5f930d9e84feb34c814ec7f0f8e2
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-21 12:01:55 +0530

    in password paper and salt added and toke generated

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
         required: true
     },
     date: {
-        type: String,
+        type: Date,
         default: Date.now
     },
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,17 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
-const user = User
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+// const user = User
+
+
+const JWT_SECRET = 'Gauravisagoodb$oy'
+
+
+
+
+
 
 // Creat a user using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
@@ -26,19 +36,28 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry A user with this email already exists" })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt)
+
+        // create a new user 
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
         })
 
-        // .then(user => res.json(user))
-        // .catch(err => {
-        //     console.log(err)
-        //     res.json({ error: "please enter valid email id", message: err.message })
-        // })
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
 
-        res.json(user)
+        const authtoken = jwt.sign(data, JWT_SECRET)
+
+
+        // res.json(user)
+        res.json({ authtoken })
 
     } catch (error) {
         console.error(error.message);
