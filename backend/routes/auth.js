bfb1793dd5bc015afe2534531706eb241e9ab00d
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-21 12:55:31 +0530

    login created and authtoken getting

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -52,12 +52,11 @@ router.post('/createuser', [
                 id: user.id
             }
         }
-
         const authtoken = jwt.sign(data, JWT_SECRET)
+        res.json({ authtoken })
 
 
         // res.json(user)
-        res.json({ authtoken })
 
     } catch (error) {
         console.error(error.message);
@@ -65,6 +64,68 @@ router.post('/createuser', [
     }
 
 })
+
+
+
+
+
+
+
+
+
+// Creat a user using: POST "/api/auth/login". No login required
+
+router.post('/login', [
+    body('email', "Enter a valid Email id").isEmail(),
+    body('password', "Password cannot be blank").exists(),
+
+], async (req, res) => {
+
+    // if there are errors, return Bad request and the errors -------> 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct email and password" });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct email and password" });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        res.json({ authtoken })
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server Error!")
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router 
