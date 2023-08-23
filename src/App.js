6722429ec5360f85c1e89f67430d85cec05ace12
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-23 16:09:07 +0530

    contex and note created

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteState from './context/notes/NoteState';
 
 
 
 function App() {
   return (
     < >
-      <BrowserRouter>
-        <Navbar />
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
 
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-
-
-
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
 
 
-        </Routes>
 
-      </BrowserRouter>
 
+
+          </Routes>
+
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
