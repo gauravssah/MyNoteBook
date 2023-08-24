ab51bd065e2be599a4af7b981d704dd2ec1ab737
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-24 17:49:21 +0530

    notes updating modal created ....., working on update updated note

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -4,7 +4,7 @@ import NoteContex from '../context/notes/NoteContext';
 const NoteItem = (props) => {
     const context = useContext(NoteContex);
     const { deleteNote } = context;
-    const { note } = props;
+    const { note, updateNote } = props;
 
     return (
         <div className='col-md-3'>
@@ -14,7 +14,7 @@ const NoteItem = (props) => {
                         <h5 className="card-title">{note.title}</h5>
                         <div className="operations">
                             <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }}></i>
-                            <i className="far fa-edit"></i>
+                            <i className="far fa-edit" onClick={() => { updateNote(note) }}></i>
                         </div>
                     </div>
                     <p className="card-text">{note.description}</p>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -52,9 +52,21 @@ const NoteState = (props) => {
     }
 
     // Delete a Note
-    const deleteNote = (id) => {
-        // Todo Api call
-        console.log("deleting the note with id" + id);
+    const deleteNote = async (id) => {
+        // Api call
+
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': "application/json",
+                "auth-token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRlMzA0MzYyYjdkY2NjYThiZTFkZmQyIn0sImlhdCI6MTY5MjYxOTIxNH0.qqIavW-BClDwJpCYOxGVFmzHpvvompgIzuenelVywis'
+            },
+
+        });
+        const json = response.json();
+        console.log(json)
+
+        console.log("Deleting the note with id" + id);
         const newNotes = notes.filter((note) => { return note._id !== id })
         setNotes(newNotes)
     }
