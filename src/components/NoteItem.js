97977a39b0ace38c05c37c2d9caccd89f0803aa8
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-08-24 12:45:05 +0530

    alert added

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,8 +6,14 @@ const NoteItem = (props) => {
         <div className='col-md-3'>
             <div className="card my-3">
                 <div className="card-body">
-                    <h5 className="card-title">{note.title}</h5>
-                    <p className="card-text">{note.description} </p>
+                    <div className="d-flex justify-content-between ">
+                        <h5 className="card-title">{note.title}</h5>
+                        <div className="operations">
+                            <i className="far fa-trash-alt mx-2"></i>
+                            <i className="far fa-edit"></i>
+                        </div>
+                    </div>
+                    <p className="card-text">{note.description}</p>
                 </div>
             </div>
         </div>
